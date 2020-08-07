48101fa11141c916f6b4f13a2b39494d38729670
TARUN SHARMA <rtdigitalsolution@gmail.com>
2020-08-07 14:22:18 -0500

    checkin

--- src/app/master/all-recovery/all-recovery.component.ts
+++ src/app/master/all-recovery/all-recovery.component.ts
@@ -1,11 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AgGridAngular } from 'ag-grid-angular';
 @Component({
   selector: 'app-all-recovery',
   templateUrl: './all-recovery.component.html',
   styleUrls: ['./all-recovery.component.css']
 })
 export class AllRecoveryComponent implements OnInit {
+  columnDefs = [
+ 
+    {headerName: 'Sr. No.', field: 'srNo', width:'80'},
+    {headerName: 'Mandate ID', field: 'mandateid', width:'150'},
+    {headerName: 'Reference ID', field: 'refernceid', width:'150'},
+    {headerName: 'Amount', field: 'amount', width:'150' },
+   
+    
+];
+
+rowData = [
+    {  srNo: '1', mandateid:'ID-000238463',  refernceid: 'Ref_887643t73', amount: '3000.00'},
+    {  srNo: '2', mandateid:'ID-000238463',  refernceid: 'Ref_887643t73', amount: '3000.00'},
+    {  srNo: '3', mandateid:'ID-000238463',  refernceid: 'Ref_887643t73', amount: '3000.00'}
+];
+
+
+
+
+  showModalstatemaster: boolean;
+
+  onClickstatemaster(event) {
+    this.showModalstatemaster = true;
+    
+    }
+    
+    hidestatemaster() {
+    this.showModalstatemaster = false;
+    }
+
 
   constructor() { }
 
--- src/app/master/all-recovery/all-recovery.module.ts
+++ src/app/master/all-recovery/all-recovery.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllRecoveryComponent } from './all-recovery.component';
 import { AllRecoveryRoutingModule } from './all-recovery-routing.module';
-
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [AllRecoveryComponent],
   imports: [
     CommonModule,
-    AllRecoveryRoutingModule
+    AllRecoveryRoutingModule,
+    AgGridModule.withComponents([])
   ]
 })
 export class AllRecoveryModule { }
